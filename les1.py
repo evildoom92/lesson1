95c0e6f4faca762ca447e99e2b94b198b41b9ab8
evildoom92 <evildoom922@gmail.com>
2023-06-14 19:59:45 +0300

    decorator

--- les1.py
+++ les1.py
@@ -1,7 +1,8 @@
-def sum(n):
-    if n < 10:
-        return n
-    return sum(n // 10) + n % 10
-n = int(input())
-if n < 0: n = -n;
-print(sum(n))
+def decorator(func):
+    def wrapper():
+        return func().upper()
+    return wrapper
+@decorator
+def imy():
+    return input("Введите Ваше имя: ")
+print("Привет, ", imy())
