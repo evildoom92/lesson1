d1f8e39cf0bc628faa5069e51f0a45f9836716ba
evildoom92 <evildoom922@gmail.com>
2023-07-04 10:09:33 +0300

    Class

--- les1.py
+++ les1.py
@@ -1,8 +1,16 @@
-def decorator(func):
-    def wrapper():
-        return func().upper()
-    return wrapper
-@decorator
-def imy():
-    return input("Введите Ваше имя: ")
-print("Привет, ", imy())
+class Car:
+    def __init__(self, brand, country):
+        self.b = brand
+        self.c = country
+    def Cars(self):
+        print("Автомобиль: " + self.b + " Производство: " + self.c)
+Car1 = Car("Мерседес", "Германия")
+Car1.Cars()
+Car1 = Car("Рено", "Франция")
+Car1.Cars()
+Car1 = Car("Ситроен", "Франция")
+Car1.Cars()
+Car1 = Car("Хонда", "Япония")
+Car1.Cars()
+Car1 = Car("Хюндай", "Япония")
+Car1.Cars()
