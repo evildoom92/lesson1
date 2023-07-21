54a410dc6625a45c554a00dbd798d140e1e67818
evildoom92 <evildoom922@gmail.com>
2023-07-21 21:37:57 +0300

    animal class

--- les1.py
+++ les1.py
@@ -1,16 +1,16 @@
-class Car:
-    def __init__(self, brand, country):
-        self.b = brand
-        self.c = country
-    def Cars(self):
-        print("Автомобиль: " + self.b + " Производство: " + self.c)
-Car1 = Car("Мерседес", "Германия")
-Car1.Cars()
-Car1 = Car("Рено", "Франция")
-Car1.Cars()
-Car1 = Car("Ситроен", "Франция")
-Car1.Cars()
-Car1 = Car("Хонда", "Япония")
-Car1.Cars()
-Car1 = Car("Хюндай", "Япония")
-Car1.Cars()
+class Animal:
+    def __init__(self, name, sex):
+        self.n = name
+        self.s = sex
+        self.a = 2
+    def Name(self):
+        print("Имя: " + self.n)
+    def Sex(self):
+        print("Пол: " + self.s)
+    def Age(self):
+        print("Возраст: " + str(self.a))
+Animal1 = Animal("Муся", "Девочка")
+Animal1.Name()
+Animal1.Sex()
+Animal1.Age()
+
