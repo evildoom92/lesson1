7d702abae2dc1d730a9503a4e3dc0845fe3a1161
evildoom92 <evildoom922@gmail.com>
2023-08-01 10:51:06 +0300

    animal class

--- les2.py
+++ les2.py
@@ -12,33 +12,40 @@ class Animal:
 
 
 class Dog(Animal):
-    def __init__(self, name, sex):
+    def __init__(self, name="Чарли", sex="Мальчик"):
         super().__init__(name, sex)
     def run(self):
         print("Собака бегает")
+    def Name(self):
+        print("Имя собаки: " + self.n)
 class Rabbit(Animal):
-    def __init__(self, name, sex):
+    def __init__(self, name="Боби", sex="Девочка"):
         super().__init__(name, sex)
     def jump(self):
         print("Кролик прыгает")
+    def Sex(self):
+        print("Пол кролика: " + self.s)
 
 class Cat(Animal):
-    def __init__(self, name, sex):
+    def __init__(self, name="Генри", sex="Мальчик"):
         super().__init__(name, sex)
     def sleep(self):
         print("Кошка спит")
+    def Age(self):
+        print("Возраст кошки: " + str(self.a))
 
-Dog1 = Dog("Шарик ", "Мальчик")
+Dog1 = Dog()
 Dog1.Name()
 Dog1.Sex()
 Dog1.run()
 
-Cat1 = Cat("Муся ", "Девочка")
+Cat1 = Cat("Муся ",)
 Cat1.Name()
 Cat1.Sex()
+Cat1.Age()
 Cat1.sleep()
 
-Rabbit1 = Rabbit("Федя", "Мальчик")
+Rabbit1 = Rabbit(sex="Мальчик")
 Rabbit1.Name()
 Rabbit1.Sex()
 Rabbit1.jump()
